9c6fa65edf0cf7988743ed16db13ebc275b3f9f3
busayolawumi <busayoolawumi3@gmail.com>
2023-10-04 16:22:53 -0700

    hot fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,3 +12,14 @@ def text_indentation(text):
     flag = 0
     for a in text:
         if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
