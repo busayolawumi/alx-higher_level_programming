67cd68b43cd9fc8fc3a0372fef4db3afe25b1c31
busayolawumi <busayoolawumi3@gmail.com>
2023-10-05 12:34:58 -0700

    quick fix

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,71 +1,46 @@
 #!/usr/bin/python3
-"""This module contain a function that multiplies two matrix"""
+"""
+This module contains a function that multiplies 2 matrices.
+"""
 
 
 def matrix_mul(m_a, m_b):
-    """matrix_mul function that multiplies two matrix
-    Args:
-        m_a (list of lists): first matrix
-        m_b (list of lists): second matrix
     """
-    if type(m_a) != list:
+    matrix multiplication
+    """
+    if type(m_a) is not list:
         raise TypeError("m_a must be a list")
-    if type(m_b) != list:
+    if type(m_b) is not list:
         raise TypeError("m_b must be a list")
-
-    # variables to verify if both m_a and m_b can be multiplied
-    num_colum1 = 0
-    num_row2 = 0
-
-    # Check requirements for matrix m_a
-    if m_a == []:
-        raise ValueError("m_a can't be empty")
-    for row1 in m_a:
-        if type(row1) != list:
+    for row in m_a:
+        if type(row) is not list:
             raise TypeError("m_a must be a list of lists")
-        len1 = len(m_a[0])
-        if row1 == []:
-            raise ValueError("m_a can't be empty")
-        if len1 != len(row1):
-            raise TypeError("each row of m_a must be of the same size")
-        num_colum1 = len(row1)
-        for column1 in row1:
-            if type(column1) != int and type(column1) != float:
-                raise TypeError("m_a should contain only integers or floats")
-
-    # Check requirements for matrix m_b
-    if m_b == []:
-        raise ValueError("m_b can't be empty")
-    for row2 in m_b:
-        if type(row2) != list:
+    for row in m_b:
+        if type(row) is not list:
             raise TypeError("m_b must be a list of lists")
-        len2 = len(m_b[0])
-        if row2 == []:
-            raise ValueError("m_b can't be empty")
-        if len2 != len(row2):
-            raise TypeError("each row of m_b must be of the same size")
-        num_row2 += 1
-        for column2 in row2:
-            if type(column2) != int and type(column2) != float:
+    if len(m_a) == 0 or len(m_a[0]) == 0:
+        raise ValueError("m_a can't be empty")
+    if len(m_b) == 0 or len(m_b[0]) == 0:
+        raise ValueError("m_b can't be empty")
+    for row in m_a:
+        for item in row:
+            if type(item) not in [int, float]:
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        for item in row:
+            if type(item) not in [int, float]:
                 raise TypeError("m_b should contain only integers or floats")
-
-    # Check if the multiplication is posible
-    if num_colum1 != num_row2:
+    for row in m_a:
+        if len(row) != len(m_a[0]):
+            raise TypeError("each row of m_a must be of the same size")
+    for row in m_b:
+        if len(row) != len(m_b[0]):
+            raise TypeError("each row of m_b must be of the same size")
+    if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-
-    mul_matrix = []
-
-    for row_1 in m_a:
-        l = 0
-        l_row = []
-        while l < len(m_b[0]):
-            result = 0
-            k = 0
-            for column_1 in row_1:
-                result += column_1 * m_b[k][l]
-                k += 1
-            l_row.append(result)
-            l += 1
-        mul_matrix.append(l_row)
-
-    return mul_matrix
+    result = [[0 for col in range(len(m_b[0]))] for row in range(len(m_a))]
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+    return result
--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -16,3 +16,13 @@ void print_python_string(PyObject *p)
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
