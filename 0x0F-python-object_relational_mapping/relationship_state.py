622f9a83c66bd18ecc783f9448ab300c149c6ad7
busayolawumi <busayoolawumi3@gmail.com>
2023-12-22 22:39:37 +0100

    fix

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""Lists states"""
+"""
+This script defines a City class
+to work with MySQLAlchemy ORM.
+"""
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
-
-Base = declarative_base()
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class State(Base):
-    """Class representing the states table"""
-    __tablename__ = 'states'
+class City(Base):
+    """City class
 
-    id = Column(Integer, nullable=False, primary_key=True,
-                autoincrement=True, unique=True)
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
+
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship(
-        "City",
-        cascade="all, delete-orphan",
-        backref=backref("state", cascade="all"),
-        single_parent=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
